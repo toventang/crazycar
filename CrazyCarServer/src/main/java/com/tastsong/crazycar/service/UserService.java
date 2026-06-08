d449752f0a0e7e6bd952079978be07a3b0d82de1
TastSong <TastSong@163.com>
2026-06-08 11:03:50 +0800

    feat(user): 添加事务管理以确保用户更新操作的原子性

--- CrazyCarServer/src/main/java/com/tastsong/crazycar/service/UserService.java
+++ CrazyCarServer/src/main/java/com/tastsong/crazycar/service/UserService.java
@@ -7,6 +7,7 @@ import com.tastsong.crazycar.mapper.UserMapper;
 import com.tastsong.crazycar.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -73,6 +74,7 @@ public class UserService {
         return !ObjUtil.isEmpty(userMapper.selectById(uid));
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public void updateUser(UserModel userModel){
         updateUserStar(userModel.getUid(), userModel.getStar());
         updateUserVip(userModel.getUid(), userModel.is_vip());
